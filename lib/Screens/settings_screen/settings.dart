f937e8e1a84d6a9f772f0c92511ce8b14c4fa7ec
Mohamed Ramadan <MoRm9n@gmail.com>
2022-10-29 16:37:29 +0200

    add Terms And conditions to Settings add Support Link to Whatsapp

--- lib/Screens/settings_screen/settings.dart
+++ lib/Screens/settings_screen/settings.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:masrufat/Providers/accounts_provider.dart';
 import 'package:masrufat/dialog/custom_generic_dialog.dart';
 import 'package:masrufat/helper/app_config.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Settings extends StatelessWidget {
   const Settings({Key? key}) : super(key: key);
@@ -25,9 +28,41 @@ class Settings extends StatelessWidget {
         },
       );
 
+  Future<void> _launchUrl(Uri url) async {
+    if (await canLaunchUrl(url)) {
+      launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> OpenWhatsAPP() async {
+    String whatsapp = '+201281100168';
+    var whatsAppURlAndroid = Uri.parse('whatsapp://send?phone=$whatsapp');
+    var whatAppURLIos = Uri.parse('https://wa.me/$whatsapp');
+    if (Platform.isIOS) {
+      // for iOS phone only
+      if (await canLaunchUrl(
+        whatAppURLIos,
+      )) {
+        await launchUrl(
+          whatAppURLIos,
+        );
+      }
+    } else {
+      // android , web
+      if (await canLaunchUrl(whatsAppURlAndroid)) {
+        await launchUrl(whatsAppURlAndroid);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final myProvider = Provider.of<AccountsProvider>(context, listen: false);
+    final terms = Uri.parse(
+      'https://www.freeprivacypolicy.com/live/ae71d61d-af62-4dee-9754-b0b3dc083581',
+    );
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -39,21 +74,25 @@ class Settings extends StatelessWidget {
               myProvider: myProvider,
             ),
           ),
-          _getListTile(
-            title: AppConfig.rateUs,
-            icon: Icons.favorite_outline,
-            onTap: () {},
-          ),
-          _getListTile(title: AppConfig.share, icon: Icons.share, onTap: () {}),
+          // _getListTile(
+          //   title: AppConfig.rateUs,
+          //   icon: Icons.favorite_outline,
+          //   onTap: () {},
+          // ),
+          // _getListTile(
+          //   title: AppConfig.share,
+          //   icon: Icons.share,
+          //   onTap: () {},
+          // ),
           _getListTile(
             title: AppConfig.support,
             icon: Icons.support_agent,
-            onTap: () {},
+            onTap: OpenWhatsAPP,
           ),
           _getListTile(
             title: AppConfig.termsAndConditions,
             icon: Icons.check_box_outlined,
-            onTap: () {},
+            onTap: () async => _launchUrl(terms),
           ),
         ],
       ),
